711f9025893f5115bbc8f83dc454c7a1cb83f3ff
istfer <fer.istem@gmail.com>
2019-09-05 05:29:27 -0400

    flag for temporary leaf water pool

--- sipnet.c
+++ sipnet.c
@@ -79,6 +79,10 @@
 // does water from the top layer drain down into bottom layer even if top layer not overflowing?
 // if litter water is off, then litter water drainage is off: litter water drainage wouldn't do anything
 
+#define LEAF_WATER 1 && (COMPLEX_WATER)
+// calculate leaf pool and evaporate from that pool 
+// makes immediate evaporation more realistic for smaller timesteps
+
 #define SNOW (1 || (COMPLEX_WATER)) && MODEL_WATER
 // keep track of snowpack, rather than assuming all precip. is liquid
 // note: when using a complex water submodel, we ALWAYS keep track of snowpack
