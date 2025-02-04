55a77e4a99e4160e84978a94dcd3726b9a9b97c0
Alomir <mike.longfritz@gmail.com>
2025-02-04 13:50:05 -0500

    Updates exit code defs

--- exitCodes.h
+++ exitCodes.h
@@ -5,8 +5,14 @@
 // get better codes, which will appear here
 typedef enum {
   EXIT_CODE_SUCCESS = 0,
-  EXIT_CODE_FAILURE = 1,
-  EXIT_CODE_UNKNOWN_EVENT = 2
+  EXIT_CODE_FAILURE = 1, // generic failure
+  // code 2 typically has a special meaning, see below
+  EXIT_CODE_UNKNOWN_EVENT = 3
 } exit_code_t;
+
+// Note for future: these codes typically have other meanings and should not be
+// used here (except for 0 and 1, for which our meaning is the special meaning)
+// 0-2, 126-165, and 255
+// See, e.g., https://tldp.org/LDP/abs/html/exitcodes.html
 
 #endif
