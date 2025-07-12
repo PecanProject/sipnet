2c73bd231ff2889f4db46fe16fc7f61ce1121b14
Chris Black <chris@ckblack.org>
2025-07-12 01:34:13 -0700

    report commit in version string

--- src/sipnet/version.h
+++ src/sipnet/version.h
@@ -1,6 +1,15 @@
 #ifndef SIPNET_VERSION_H
 #define SIPNET_VERSION_H
 
-#define VERSION_STRING "2.0.0"
+#define NUMERIC_VERSION "2.0.0"
+
+// To enable, compile with `-DGIT_HASH="<your revision here>"`
+#ifdef GIT_HASH
+#define str(x) #x
+#define xstr(x) str(x)  // bah, macro string expansion rules
+#define VERSION_STRING NUMERIC_VERSION " (" xstr(GIT_HASH) ")"
+#else
+#define VERSION_STRING NUMERIC_VERSION
+#endif  // GIT_HASH
 
 #endif  // SIPNET_VERSION_H
