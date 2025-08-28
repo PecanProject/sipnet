04b7a9f844cf25d4b188a288c2885f2aaacbbf57
Alomir <Alomir@users.noreply.github.com>
2025-08-28 21:25:00 +0000

    deploy: 345bd13e750f9a8269968a9a246ae63add394c42

--- api/search/functions_11.js
+++ api/search/functions_11.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['updatecharcontext_0',['updatecharcontext',['../d8/dfa/context_8c.html#a98c6509e4642073a88bc9e2ffcb83a8f',1,'updateCharContext(const char *name, const char *value, context_source_t source):&#160;context.c'],['../dc/d1e/context_8h.html#a98c6509e4642073a88bc9e2ffcb83a8f',1,'updateCharContext(const char *name, const char *value, context_source_t source):&#160;context.c']]],
   ['updateintcontext_1',['updateintcontext',['../d8/dfa/context_8c.html#a0f4f1dc481cb817a031def2778f6a75b',1,'updateIntContext(const char *name, int value, context_source_t source):&#160;context.c'],['../dc/d1e/context_8h.html#a0f4f1dc481cb817a031def2778f6a75b',1,'updateIntContext(const char *name, int value, context_source_t source):&#160;context.c']]],
-  ['updatestate_2',['updateState',['../dd/d09/sipnet_8c.html#a972eedbc68ae911757b8efa8952aa1fc',1,'sipnet.c']]],
-  ['updatetrackers_3',['updateTrackers',['../dd/d09/sipnet_8c.html#a8b7178c18acaf22dffc942561473d6bb',1,'sipnet.c']]],
-  ['usage_4',['usage',['../dd/def/cli_8c.html#af3fc073cbe5282a8dcb2a1380512b3e8',1,'usage(char *progName):&#160;cli.c'],['../dc/db0/cli_8h.html#af3fc073cbe5282a8dcb2a1380512b3e8',1,'usage(char *progName):&#160;cli.c']]]
+  ['updatepoolsforevents_2',['updatePoolsForEvents',['../dd/d09/sipnet_8c.html#ac9de3738992b3780a28e1bdc3eba6604',1,'sipnet.c']]],
+  ['updatestate_3',['updateState',['../dd/d09/sipnet_8c.html#a972eedbc68ae911757b8efa8952aa1fc',1,'sipnet.c']]],
+  ['updatetrackers_4',['updateTrackers',['../dd/d09/sipnet_8c.html#a8b7178c18acaf22dffc942561473d6bb',1,'sipnet.c']]],
+  ['usage_5',['usage',['../dd/def/cli_8c.html#af3fc073cbe5282a8dcb2a1380512b3e8',1,'usage(char *progName):&#160;cli.c'],['../dc/db0/cli_8h.html#af3fc073cbe5282a8dcb2a1380512b3e8',1,'usage(char *progName):&#160;cli.c']]]
 ];
