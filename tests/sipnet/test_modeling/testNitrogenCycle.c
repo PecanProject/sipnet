52aa17907ffedb52965116e437a34e819a759962
Mike Longfritz <Mike.Longfritz@gmail.com>
2025-10-22 14:40:56 -0400

    Adds test for N cycle modeling

--- tests/sipnet/test_modeling/testNitrogenCycle.c
+++ tests/sipnet/test_modeling/testNitrogenCycle.c
@@ -1,52 +1,151 @@
 #include <stdio.h>
 
 #include "utils/tUtils.h"
-#include "utils/exitHandler.c"
 #include "sipnet/events.c"
+#include "sipnet/sipnet.c"
 
-// These test points check the fix for issue
-// https://github.com/PecanProject/sipnet/issues/74
+void setupTests(void) {
+  // set up dummy climate
+  climate = (ClimateNode *)malloc(sizeof(ClimateNode));
+  climate->year = 2024;
+  climate->day = 70;
+  climate->length = 0.125;
+  climate->tsoil = 20.0;
+
+  // Set up the context
+  initContext();
+  ctx.litterPool = 1;
+  ctx.nitrogenCycle = 1;
+}
+
+void initState(double initN, double nVol) {
+  // per test
+  envi.minN = initN;
+  params.nVolatilization = nVol;
+
+  // static
+  envi.soilWater = 5.0;
+  envi.soil = 1.5;
+  envi.litter = 1;
+
+  params.minNInit = 0.0;
+  params.soilWHC = 10.0;
+
+  fluxes.nVolatilization = 0;
+
+  // Values from russell_2 smoke test
+  params.soilRespMoistEffect = 1.0;
+  params.baseSoilResp = 0.06;
+  params.soilRespQ10 = 2.9;
+}
+
+int checkFlux(double expNVol) {
+  int status = 0;
+  double expFlux = expNVol / climate->length;
+  if (!compareDoubles(fluxes.nVolatilization, expFlux)) {
+    status = 1;
+    logTest("N volatilization flux is %f, expected %f\n",
+            fluxes.nVolatilization, expFlux);
+  }
+
+  return status;
+}
+
+int testNVol(void) {
+  int status = 0;
+  double minN;
+  double nVol;
+  double expNVol;
+
+  minN = 2;
+  nVol = 0.1;
+  initState(minN, nVol);
+  double tEffect = calcTempEffect(climate->tsoil);
+  double mEffect = calcMoistEffect(envi.soilWater, params.soilWHC);
+  expNVol = nVol * minN * tEffect * mEffect;
+  calcNVolatilizationFlux();
+  status |= checkFlux(expNVol);
+
+  // easy proportionality test - doubling params should double output
+  minN *= 2;
+  expNVol *= 2;
+  initState(minN, nVol);
+  calcNVolatilizationFlux();
+  status |= checkFlux(expNVol);
+
+  expNVol *= 2;
+  initState(minN, nVol);
+  params.baseSoilResp *= 2;  // should double tEffect
+  calcNVolatilizationFlux();
+  status |= checkFlux(expNVol);
+
+  // Check minN for the last
+  updatePoolsForSoil();
+  double expMinN = minN - expNVol;
+  int minStatus = 0;
+  if (!compareDoubles(envi.minN, expMinN)) {
+    minStatus = 1;
+    logTest("minN pool is %8.3f, expected %8.3f\n", envi.minN, expMinN);
+  }
+  status |= minStatus;
+
+  return status;
+}
+
+int testFert(void) {
+  int status = 0;
+  double initN = 2.0;
+  double nVolFrac = 0.1;
+  double expMinN, expEventMinN, expNVol;
+
+  // init minN 2, nVol 0.1
+  initState(initN, nVolFrac);
+
+  // fert event: 15 5 10
+  double fertMinN = 10;
+  initEvents("events_fert.in", 0);
+  setupEvents();
+
+  calcNVolatilizationFlux();
+  processEvents();
+  updatePoolsForSoil();
+  updatePoolsForEvents();
+
+  // Want to test:
+  // envi: minN
+  // fluxes: nVol, eventMinN
+  double tEffect = calcTempEffect(climate->tsoil);
+  double mEffect = calcMoistEffect(envi.soilWater, params.soilWHC);
+  expNVol = (nVolFrac * initN * tEffect * mEffect) / climate->length;
+  expEventMinN = fertMinN / climate->length;
+  expMinN = initN + (expEventMinN - expNVol) * climate->length;
+
+  if (!compareDoubles(fluxes.nVolatilization, expNVol)) {
+    status = 1;
+    logTest("fluxes.nVolatilization is %8.4f, expected %8.4f\n",
+            fluxes.nVolatilization, expNVol);
+  }
+  if (!compareDoubles(fluxes.eventMinN, expEventMinN)) {
+    status = 1;
+    logTest("fluxes.eventMinN is %8.4f, expected %8.4f\n", fluxes.eventMinN,
+            expEventMinN);
+  }
+  if (!compareDoubles(envi.minN, expMinN)) {
+    status = 1;
+    logTest("envi.minN is %8.4f, expected %8.4f\n", envi.minN, expMinN);
+  }
+
+  return status;
+}
 
 int run(void) {
   int status = 0;
 
-  // Set up the context
-  initContext();
-  updateIntContext("events", 1, CTX_TEST);
+  setupTests();
 
-  // exit() handling params
-  int jmp_rval;
+  status |= testFert();
 
-  // First test - this should pass, not exit
-  should_exit = 0;
-  exit_result = 1;
-  expected_code = EXIT_CODE_INPUT_FILE_ERROR;
-  jmp_rval = setjmp(jump_env);
-  if (!jmp_rval) {
-    readEventData("infra_events_year_boundary.in");
-  }
-  test_assert(jmp_rval == 0);
-  status |= !exit_result;
-  if (!exit_result) {
-    logTest("FAIL with infra_events_year_boundary.in\n");
-  }
-
-  // Second test - this should exit
-  should_exit = 1;
-  exit_result = 1;  // reset for next test
-  expected_code = EXIT_CODE_INPUT_FILE_ERROR;
-  jmp_rval = setjmp(jump_env);
-  if (!jmp_rval) {
-    readEventData("infra_events_bad_order.in");
-  }
-  test_assert(jmp_rval == 1);
-  status |= !exit_result;
-  if (!exit_result) {
-    logTest("FAIL with infra_events_bad_order.in\n");
-  }
-
-  // Allow a real exit, not that this is really needed
-  really_exit = 1;
+  status |= testNVol();
 
   return status;
 }
@@ -54,13 +153,12 @@ int run(void) {
 int main(void) {
   int status;
 
-  logTest("Starting testEventFileOrderChecks:run()\n");
+  logTest("Starting testNitrogenCycle:run()\n");
   status = run();
   if (status) {
-    really_exit = 1;
-    logTest("FAILED testEventFileOrderChecks with status %d\n", status);
+    logTest("FAILED testNitrogenCycle with status %d\n", status);
     exit(status);
   }
 
-  logTest("PASSED testEventFileOrderChecks\n");
+  logTest("PASSED testNitrogenCycle\n");
 }
