6f77a31e29a67f7d60f8d2f9a09bef38bf951be2
Mike Longfritz <Mike.Longfritz@gmail.com>
2025-11-25 12:08:48 -0500

    Initial implementation for organic N pools plus fert updates

--- src/sipnet/state.h
+++ src/sipnet/state.h
@@ -340,7 +340,7 @@ typedef struct Parameters {
   // autotrophic respiration:
   // growth resp. as fraction of (GPP - woodResp - folResp)
   // Note: that comment may not be correct, growthResp is calc'd as
-  //       (mean GPP) * (growtheRespFrac)
+  //       (mean GPP) * (growthRespFrac)
   //       with no correction for woodResp or folResp
   double growthRespFrac;
 
@@ -370,8 +370,17 @@ typedef struct Parameters {
   // Fraction of mineral N available to be volatilized per day, d^-1
   double nVolatilizationFrac;
 
-  // Fraction of mineral N lost to leaching per day
+  // Fraction of mineral N available to be leached, unitless
   double nLeachingFrac;
+
+  // C:N ratio for leaves, assumed static, g C/g N
+  double leafCNRatio;
+
+  // C:N ratio for wood (and coarse roots), assumed static, g C/g N
+  double woodCNRatio;
+
+  // C:N ratio for fine roots, assumed static, g C/g N
+  double rootCNRatio;
 
 } Params;
 
@@ -543,9 +552,17 @@ typedef struct FluxVars {
   //
 
   // Mineral N lost to volatilization
+  // g N * m^-2 ground area * day^-1
   double nVolatilization;
   // Mineral N lost to leaching
+  // g N * m^-2 ground area * day^-1
   double nLeaching;
+  // Organic N flux for the soil organic N pool
+  // g N * m^-2 ground area * day^-1
+  double nOrgSoil;
+  // Organic N flux for the litter N pool
+  // g N * m^-2 ground area * day^-1
+  double nOrgLitter;
 
   // ****************************************
   // Fluxes for event handling
