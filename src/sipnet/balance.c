0ec16f71a8bf3adba33d688616142cb04cfc60ad
Mike Longfritz <Mike.Longfritz@gmail.com>
2026-02-03 11:42:54 -0500

    Carbon and nitrogen balance checks

--- src/sipnet/balance.c
+++ src/sipnet/balance.c
@@ -1,3 +1,86 @@
-//
-// Created by Michael J Longfritz on 1/30/26.
-//
+#include "balance.h"
+#include "common/context.h"
+#include "state.h"
+
+// Definition of global balance tracker struct
+BalanceTracker balanceTracker;
+
+void getMassTotals(double *carbon, double *nitrogen) {
+  *carbon = envi.plantWoodC + envi.plantLeafC + envi.fineRootC +
+            envi.coarseRootC + envi.soilC + envi.nppStorage;
+  if (ctx.litterPool) {
+    *carbon += envi.litterC;
+  }
+
+  if (ctx.nitrogenCycle) {
+    *nitrogen =
+        envi.plantWoodC / params.woodCN + envi.plantLeafC / params.leafCN +
+        envi.fineRootC / params.fineRootCN + envi.coarseRootC / params.woodCN +
+        envi.soilOrgN + envi.litterN + envi.minN;
+  } else {
+    *nitrogen = 0.0;
+  }
+}
+
+void updateBalanceTrackerPreUpdate(void) {
+  // Set the pre-update pool totals
+  getMassTotals(&balanceTracker.preTotalC, &balanceTracker.preTotalN);
+}
+
+void updateBalanceTrackerPostUpdate(void) {
+  // Set the post-update pool totals
+  getMassTotals(&balanceTracker.postTotalC, &balanceTracker.postTotalN);
+
+  // Calculate the system inputs and outputs
+  // CARBON
+  balanceTracker.inputsC = fluxes.photosynthesis +  // GPP
+                           fluxes.eventInputC;  // agro event additions
+  balanceTracker.outputsC = fluxes.rVeg + fluxes.rFineRoot +
+                            fluxes.rCoarseRoot +  // R_a
+                            fluxes.rSoil +  // R_h
+                            fluxes.eventOutputC;  // agro event removals
+  if (ctx.litterPool) {
+    balanceTracker.outputsC += fluxes.rLitter;
+  }
+  // Account for climate length
+  balanceTracker.inputsC *= climate->length;
+  balanceTracker.outputsC *= climate->length;
+
+  // NITROGEN
+  if (ctx.nitrogenCycle) {
+    balanceTracker.inputsN =
+        // TODO: fluxes.fixation +
+        fluxes.eventInputN;
+    balanceTracker.outputsN =
+        fluxes.nLeaching + fluxes.nVolatilization + fluxes.eventOutputN;
+
+    // Account for climate length
+    balanceTracker.inputsN *= climate->length;
+    balanceTracker.outputsN *= climate->length;
+  }
+}
+
+void initBalanceTracker(double tolerance) {
+  // Initialize all to zero
+  memset(&balanceTracker, 0, sizeof(balanceTracker));
+  // And then set tolerance
+  balanceTracker.tolerance = tolerance;
+}
+
+void checkBalance(void) {
+  // CARBON
+  // Pool delta
+  double poolCDelta = balanceTracker.postTotalC - balanceTracker.preTotalC;
+  // System delta
+  double systemCDelta = balanceTracker.inputsC - balanceTracker.outputsC;
+  balanceTracker.deltaC = poolCDelta - systemCDelta;
+
+  // NITROGEN
+  // Pool delta
+  double poolNDelta = balanceTracker.postTotalN - balanceTracker.preTotalN;
+  // System delta
+  double systemNDelta = balanceTracker.inputsN - balanceTracker.outputsN;
+  balanceTracker.deltaN = poolNDelta - systemNDelta;
+
+  // TBD: warn if balance off?
+}
---
+++ src/sipnet/balance.h
@@ -0,0 +1,42 @@
+#ifndef BALANCE_H
+#define BALANCE_H
+
+typedef struct BalanceTrackerStruct {
+  // Mass balance checks:
+  //   X_t - X_(t-1) = inputs - outputs + tolerance
+  // or, we check that
+  //   (pool delta) + (system delta) < tolerance
+  // where
+  //   pool delta = X_t - X_(t-1)
+  //   system delta = outputs - inputs
+
+  // Carbon balance
+  double preTotalC;
+  double postTotalC;
+  double inputsC;
+  double outputsC;
+
+  // Nitrogen balance
+  double preTotalN;
+  double postTotalN;
+  double inputsN;
+  double outputsN;
+
+  // Tolerance and checks
+  double tolerance;  // TBD: this may need to be dynamic
+  double deltaC;
+  double deltaN;
+} BalanceTracker;
+
+// Global var
+extern BalanceTracker balanceTracker;
+
+void updateBalanceTrackerPreUpdate(void);
+
+void updateBalanceTrackerPostUpdate(void);
+
+void initBalanceTracker(double tolerance);
+
+void checkBalance(void);
+
+#endif  // BALANCE_H
