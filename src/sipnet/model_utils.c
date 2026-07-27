caaf3de90d6b36fe414621dae793af0ea9a39edc
Mike Longfritz <Mike.Longfritz@gmail.com>
2026-07-27 11:19:39 -0400

    Common location for model-specific utils

--- src/sipnet/model_utils.c
+++ src/sipnet/model_utils.c
@@ -1,1 +1,16 @@
-#include "carbon.h"
+#include "model_utils.h"
+
+#include "common/logging.h"
+#include "common/util.h"
+#include "state.h"
+
+void ensureNonNegative(double *var, double minVal, const char *label) {
+  if (*var < minVal) {
+    if (*var < -EPS) {  // Don't print the zeros-except-for-roundoff
+      logWarning("Non-negative stock constraint applied for %s (value %g set "
+                 "to zero) year %d day %d time %6.3f\n",
+                 label, *var, climate->year, climate->day, climate->time);
+    }
+    *var = 0.;
+  }
+}
--- src/sipnet/model_utils.h
+++ src/sipnet/model_utils.h
@@ -1,4 +1,22 @@
-#ifndef SIPNET_CARBON_H
-#define SIPNET_CARBON_H
+#ifndef MODEL_UTILS_H
+#define MODEL_UTILS_H
 
-#endif  // SIPNET_CARBON_H
+// Utilities for SIPNET that depend on the model
+//
+// Model-agnostic utilities should go in common/utils.h
+
+/**
+ * Check that input var is not less than input threshold minVal
+ *
+ * Checks that var >= minVal; if not, set var to zero and issue a warning.
+ *
+ * Note that if minVal = 0, then this will (as suggested) ensure that var >= 0
+ * If minVal > 0, then minVal can be thought of as some epsilon value, below
+ *
+ * @param var double-valued variable to check
+ * @param minVal
+ * @param label
+ */
+void ensureNonNegative(double *var, double minVal, const char *label);
+
+#endif  // MODEL_UTILS_H
